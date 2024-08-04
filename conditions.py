b85b621e44e38788a4283b56ed1887f3faa8242e
bablidanu <158181563+bablidanu@users.noreply.github.com>
2024-08-04 12:46:38 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number :  "))
+b = int(input("Enter the second number :  "))
+if a < b :
+    print(" a is less than b")
+elif a == b : 
+    print("a is equal to b ")
+else:
+    print(" a is greater than b")
