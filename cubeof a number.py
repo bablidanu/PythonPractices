c42d4e47bb931d2e3176cd32d4c926fd66327f04
Babli Danu <158181563+bablidanu@users.noreply.github.com>
2024-08-08 06:19:36 +0530

    Add files via upload

---
+++ cubeof a number.py
@@ -0,0 +1,11 @@
+def cube(x):
+    return x*x*x
+
+print(cube(3))
+print(cube(2))
+print(cube(4))
+print(cube(5))
+
+
+
+
---
+++ square of a list numbers.py
@@ -0,0 +1,3 @@
+l = [1,2,3,4,5]
+newl = list(map(lambda x : x*x,l))
+print(newl)
